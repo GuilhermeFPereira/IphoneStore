2c601c86cda182c22c24123aa24c9ad7c9f11771
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-03-20 09:47:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeColor (cor){
+    let circulo = document.querySelector(".circulo") 
+    circulo.style.background = cor
+}
+
+function changeImg (img){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src =  img
+}
